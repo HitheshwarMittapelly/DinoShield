ab95de314faa52e0d89d0fd734fa533dca6d18ae
Rohan <rohanpatel1899@gmail.com>
2019-01-15 15:23:27 -0700

    Added collision and random falling of asteroids

---
+++ DinoShield/Assets/Scripts/DropHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropHandler : MonoBehaviour {
+
+    private GameObject[] Asteroids = new GameObject[14];
+    private DropIt[] TriggerDrop = new DropIt[14];
+
+    private IEnumerator coroutine;
+    bool canDrop = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        int children = transform.childCount;
+        for (int i = 0; i < children; i++)
+        {
+            Asteroids[i] = transform.GetChild(i).gameObject;
+            TriggerDrop[i] = Asteroids[i].GetComponent<DropIt>();
+        }
+
+        //foreach (Transform child in transform)
+        //{       print("Foreach loop: " + child);
+        //    Asteroids[i] = GetComponentInChildren<DropIt>().gameObject;
+        //    //TriggerDrop[i] = GetComponentInChildren<DropIt>();
+        //}
+    }
+	
+    // Update is called once per frame
+	void Update () {
+       if(canDrop)
+        StartCoroutine(RandDrop(4.0f));
+	}
+
+    private IEnumerator RandDrop(float waitTime) {
+        canDrop = false;
+        int toDrop = Random.Range(0, 13);
+       // Debug.Log(toDrop);
+        TriggerDrop[toDrop].DropItBaby();
+        yield return new WaitForSeconds(waitTime);
+        canDrop = true;
+    }
+  
+}
---
+++ DinoShield/Assets/Scripts/DropIt.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropIt : MonoBehaviour {
+
+    public Rigidbody rb;
+    Vector3 initPos,currentPos;
+    Quaternion initRot;
+    bool IsDropping = false;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        initPos = this.transform.position;
+        initRot = this.transform.rotation;
+    }
+
+    private void Update()
+    {
+        //if (!IsDropping)
+        //{
+        //    this.transform.SetPositionAndRotation(currentPos,initRot);   
+        //}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if it collides with egg
+        if (collision.gameObject.tag == "Egg")
+        {
+            IsDropping = false;
+            Debug.Log("Collision with Egg");
+            // decrease the score/lives
+          
+            // go back to init position
+            rb.velocity = new Vector3(0,0,0);
+            currentPos = initPos;
+         
+        }
+
+        // if it collides with plate
+        if (collision.gameObject.tag == "shield")
+        {
+            Debug.Log("Collision with plate");
+            // increase the score
+
+            // go back to init position
+            currentPos = initPos;
+        }
+    }
+
+    public void DropItBaby() {
+        IsDropping = true;
+        rb.velocity = new Vector3(0,-2,0);
+    }
+
+}
