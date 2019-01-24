84ffd41ca82981d121101a5654b657769f8d8681
Joshua Dudleston <Zrovom@gmail.com>
2019-01-23 18:44:53 -0700

    scriupt

---
+++ DinoShield/Assets/Scripts/AnimScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimScript : MonoBehaviour
+
+{
+
+    Animator animator;
+    public char key;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(""+key))
+            animator.SetTrigger("On");
+        if (Input.GetKeyUp(""+key))
+            animator.SetTrigger("Off");
+    }
+
+    
+}
